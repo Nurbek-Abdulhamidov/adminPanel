ec86c171d4f21b085ef03cdc7f69b8b2203efdf9
nurbek <abduhamidovnurbek888@gmail.com>
2022-03-25 20:07:58 +0500

    Table adedd

---
+++ src/components/Widgets/Widgets.jsx
@@ -0,0 +1,76 @@
+import "./Widgets.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import AccountBalanceOutlinedIcon from "@mui/icons-material/AccountBalanceOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+
+const Widgets = ({ type }) => {
+  let data;
+
+  let amount = 100;
+  let diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "USERS",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlineOutlinedIcon
+            className="icon"
+            style={{ color: "crimson", backgroundColor: "rgba(255,0,0,0.2)" }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "ORDERS",
+        isMoney: false,
+        link: "View all orders",
+        icon: <ShoppingCartOutlinedIcon className="icon"  style={{ color: "crimson", backgroundColor: "#ffee00dd" }}/>,
+      };
+      break;
+    case "earning":
+      data = {
+        title: "EARNINGS",
+        isMoney: true,
+        link: "View net earnings",
+        icon: <MonetizationOnOutlinedIcon className="icon" style={{ color: "crimson", backgroundColor: "#1eff00a2" }} />,
+      };
+      break;
+    case "balance":
+      data = {
+        title: "BALANCE",
+        isMoney: true,
+        link: "See details",
+        icon: <AccountBalanceOutlinedIcon className="icon" style={{ color: "crimson", backgroundColor: "#8814f584" }}/>,
+      };
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div className="widget">
+      <div className="left">
+        <div className="title">{data.title}</div>
+        <div className="counter">
+          {data.isMoney && "$"} {amount}
+        </div>
+        <div className="link">{data.link}</div>
+      </div>
+      <div className="right">
+        <div className="percantage positive">
+          <KeyboardArrowUpIcon />
+          {diff} %
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
+export default Widgets;
