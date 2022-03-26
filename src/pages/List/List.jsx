0f26d96c63c1d6e9fc8f3da769a76a8d7c81eb9a
nurbek <abduhamidovnurbek888@gmail.com>
2022-03-26 19:44:41 +0500

    Table section added

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,9 +1,17 @@
-import './List.scss'
-
+import "./List.scss";
+import Sidebar from "../../components/SideBar/Sidebar";
+import Navbar from "../../components/Navbar/Navbar";
+import Datatable from "../../components/datatable/Datatable";
 const List = () => {
   return (
-    <div>List</div>
-  )
-}
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <Datatable />
+      </div>
+    </div>
+  );
+};
 
-export default List
+export default List;
